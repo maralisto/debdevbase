1afdfe83ddf2921fa5cd1a49f28c8aad0c163901
Marco A. Hudelist <marco@hudelist.org>
2023-10-07 20:14:27 +0200

    First dummy version

---
+++ debdevbase.sh
@@ -0,0 +1,12 @@
+echo "Welcome to maralisto's base install script for a development environment for execution in a VM!"
+read -p  "Please make sure that the script is sudo-ed (type: yes): " sudostatus
+
+if [ $sudostatus != 'yes' ]; then
+	echo "Abort..."
+	exit -1
+fi
+
+echo "Starting installing..."
+	
+
+
