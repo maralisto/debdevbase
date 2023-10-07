ca254a2eb2ffa91655232211bcd2d2b862fff718
Marco A. Hudelist <marco@hudelist.org>
2023-10-07 20:46:48 +0200

    noninteractive install

--- debdevbase.sh
+++ debdevbase.sh
@@ -7,7 +7,7 @@ if [ $sudostatus != 'yes' ]; then
 fi
 
 echo "Starting installing..."
-
+export DEBIAN_FRONTEND=noninteractive
 apt update
 apt upgrade --yes
 apt install --yes git nala lightdm i3 nitrogen polybar -y
